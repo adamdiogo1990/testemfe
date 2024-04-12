8267e48f9562f19e517a728a0dfba715ac544058
adamsouzaloopt <adam.souza@loopt.com.br>
2024-04-12 17:48:05 -0300

    teste2

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,11 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, NgZone } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { APP_ROUTES } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(APP_ROUTES)]
+  providers: [provideRouter(APP_ROUTES),{
+    provide: NgZone,
+    useValue: new NgZone({ shouldCoalesceEventChangeDetection: false })
+},]
 };
--- src/bootstrap.ts
+++ src/bootstrap.ts
@@ -1,4 +1,4 @@
-import 'zone.js/dist/zone';
+import 'zone.js';
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
